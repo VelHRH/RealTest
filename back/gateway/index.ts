cf9a33bd5268589e0fb1546e1a74ba7efc01d8c0
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-04-29 11:12:34 +0300

    uploading files

--- back/gateway/index.ts
+++ back/gateway/index.ts
@@ -2,16 +2,40 @@ import express from "express";
 import cors from "cors";
 import proxy from "express-http-proxy";
 import { config } from "dotenv";
+import multer from "multer";
 
 config();
 const app = express();
 
 app.use(cors());
+
+const storage = multer.diskStorage({
+ destination: (_, __, cb) => {
+  cb(null, "uploads");
+ },
+ filename: (_, file, cb) => {
+  cb(null, file.originalname);
+ },
+});
+
+const upload = multer({ storage });
+
 app.use(express.json());
 
 app.use("/user", proxy("http://localhost:8001"));
 app.use("/test", proxy("http://localhost:8002"));
 app.use("/company", proxy("http://localhost:8003"));
+app.use("/uploads", express.static("uploads"));
+
+app.post(
+ "/upload",
+ upload.single("image"),
+ (req: express.Request, res: express.Response) => {
+  res.json({
+   url: `uploads/${req.file?.originalname}`,
+  });
+ }
+);
 
 app.listen(process.env.PORT, () => {
  console.log(`Gateway service working on port ${process.env.PORT}!`);
