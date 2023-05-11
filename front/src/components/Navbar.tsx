5d03101b0ca9aeae807092704f001354d658db59
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-05-11 19:38:17 +0300

    changed navbar

--- front/src/components/Navbar.tsx
+++ front/src/components/Navbar.tsx
@@ -3,25 +3,31 @@ import MenuElem from "./ui/MenuElem";
 import { cookies } from "next/headers";
 
 const Navbar = async () => {
- const user = await fetch("http://localhost:8000/user/me", {
+ const res = await fetch("http://localhost:8000/user/me", {
   headers: {
    Cookie: `COOKIE_AUTH=${cookies().get("COOKIE_AUTH")?.value}`,
   },
  });
- const data = await user.json();
+ const user = await res.json();
  return (
   <div className="w-full flex h-20 items-center justify-between">
    <Logo size="large" />
    <ul className="flex h-full items-center">
-    {data.login ? (
+    <MenuElem link="#"> companies </MenuElem>
+    <MenuElem link="#"> users </MenuElem>
+    {user.login ? (
      <>
       <MenuElem link="#"> my </MenuElem>
       <MenuElem link="#"> me </MenuElem>
+      <MenuElem link="/user/login" isLast>
+       {user.name}
+      </MenuElem>
      </>
     ) : (
      <>
-      <MenuElem link="#"> companies </MenuElem>
-      <MenuElem link="#"> users </MenuElem>
+      <MenuElem link="/user/login" isLast>
+       Login
+      </MenuElem>
      </>
     )}
    </ul>
--- front/src/components/ui/MenuElem.tsx
+++ front/src/components/ui/MenuElem.tsx
@@ -4,11 +4,18 @@ import Link from "next/link";
 interface MenuElemProps {
  children: string;
  link: string;
+ isLast?: boolean;
 }
 
-const MenuElem: FC<MenuElemProps> = ({ children, link }) => {
+const MenuElem: FC<MenuElemProps> = ({ children, link, isLast }) => {
  return (
-  <li className="text-2xl text-amber-100 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500 font-bold cursor-pointer duration-300 mr-10">
+  <li
+   className={`text-2xl ${
+    isLast ? "text-amber-400" : "text-amber-100"
+   } hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-amber-400 hover:to-amber-500 font-bold cursor-pointer duration-300 ${
+    !isLast && "mr-10"
+   }`}
+  >
    <Link href={link}>{children}</Link>
   </li>
  );
