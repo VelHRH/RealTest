0343f564da0eec250334aeeca67bd84dc6db42e0
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-05-22 20:20:31 +0300

    Results data handling

--- back/test/src/services/ResultService.ts
+++ back/test/src/services/ResultService.ts
@@ -74,10 +74,54 @@ export class ResultService {
   await newResult.save();
  }
 
+ GetDataOfResult(result: string) {
+  const cm = parseInt(result.slice(0, result.indexOf("cm")));
+  const time = parseInt(
+   result.slice(result.indexOf(" "), result.indexOf("ms"))
+  );
+  return { cm, time };
+ }
+
  async GetResults({ testId }: { testId: string }) {
   try {
    const results = await ResultModel.find({ testId });
-   return results;
+   let reports = [];
+
+   const defaultDistance = parseInt(
+    results[0].data[0].slice(0, results[0].data[0].indexOf("cm"))
+   );
+   for (let result of results) {
+    let approaches = [];
+    for (let i = 0; i < result.data.length - 1; i++) {
+     if (defaultDistance - this.GetDataOfResult(result.data[i]).cm >= 5) {
+      approaches.push({
+       approach: this.GetDataOfResult(result.data[i]).cm,
+       duration:
+        (this.GetDataOfResult(result.data[i + 1]).time -
+         this.GetDataOfResult(result.data[i]).time) /
+        1000,
+      });
+     }
+    }
+    if (
+     defaultDistance -
+      this.GetDataOfResult(result.data[result.data.length - 1]).cm >=
+     5
+    ) {
+     approaches.push({
+      approach: this.GetDataOfResult(result.data[result.data.length - 1]).cm,
+      duration: -1,
+     });
+    }
+    reports.push({
+     _id: result._id,
+     testId: result.testId,
+     approaches,
+     resultStart: result.resultStart,
+     resultEnd: result.resultEnd,
+    });
+   }
+   return reports;
   } catch (err) {
    throw err;
   }
