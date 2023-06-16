0f016fab1b5af7ff2a869a9238016006c0d9426e
Valentine Yurchenko <45362610+VelHRH@users.noreply.github.com>
2023-06-16 15:01:45 +0300

    only changing values output

--- arduino/sketch_mar27a.ino
+++ arduino/sketch_mar27a.ino
@@ -1,28 +1,38 @@
-int trigPin = 11;    // Trigger
-int echoPin = 12;    // Echo
-long duration, cm;
- 
-void setup() {
-  Serial.begin (9600);
+int trigPin = 11;
+int echoPin = 12;
+unsigned long duration, previousTime = 0;
+int curDistance, previousDistance = 0;
+
+void setup()
+{
+  Serial.begin(9600);
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 }
- 
-void loop() {
+
+void loop()
+{
   digitalWrite(trigPin, LOW);
   delayMicroseconds(5);
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
- 
+
   pinMode(echoPin, INPUT);
   duration = pulseIn(echoPin, HIGH);
 
-  cm = (duration/2) / 29.1;
-  
-  Serial.print(cm);
-  Serial.print("cm");
-  Serial.println();
-  
-  delay(1000);
+  curDistance = (duration / 2) / 29.1;
+
+  if (abs(curDistance - previousDistance) >= 5)
+  {
+    unsigned long currentTime = millis();
+    Serial.print(curDistance);
+    Serial.print("cm ");
+    Serial.print(currentTime);
+    Serial.println("ms");
+    previousDistance = curDistance;
+    previousTime = currentTime;
+  }
+
+  delay(500);
 }
